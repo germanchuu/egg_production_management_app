5ed3eb05f73d28a7f5c6f32f3fb748a3ae117b6a
Daniel <germancito.sanz@gmail.com>
2026-02-21 15:16:27 -0400

    fix: logo view stretches to full row height in home header

--- src/shared/components/home/UserHeader.tsx
+++ src/shared/components/home/UserHeader.tsx
@@ -54,7 +54,7 @@ export function UserHeader() {
         {/* Main content */}
         <View className="relative flex-row items-start gap-4">
           {/* Avatar */}
-          <View className="h-16 rounded-2xl overflow-hidden shadow-lg bg-white">
+          <View className="self-stretch rounded-2xl overflow-hidden shadow-lg bg-white">
             <AppLogo size={64} />
           </View>
 
